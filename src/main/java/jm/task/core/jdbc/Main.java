0e503d00e72c98ba3a5e478d994193f1d001dbd9
chipsbl <drozdovkin@bk.ru>
2025-03-14 15:56:00 +0300

    Ready tests and work area

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,8 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.ArrayList;
@@ -9,13 +11,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-        userDao.dropUsersTable();
-        userDao.createUsersTable();
-        userDao.saveUser("Kokonito", "Kika22rdo", (byte) 82);
-        userDao.saveUser("Fetuchini", "Drozdilini", (byte) 5);
-        List<User> users = userDao.getAllUsers();
-        users.forEach(System.out::println);
-        userDao.cleanUsersTable();
+        Util util = new Util();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Fedor", "Drozdov", (byte) 23);
+        userService.saveUser("Brad", "Pitt", (byte) 56);
+        userService.saveUser("Kirl", "Johanson", (byte) 35);
+        userService.saveUser("Crocodilo", "Bombardilo", (byte) 5);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        util.closeConnection();
     }
 }
