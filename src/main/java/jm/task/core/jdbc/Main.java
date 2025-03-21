8ebc72d81b149eb927a03745d2d237f14a46b992
chipsbl <drozdovkin@bk.ru>
2025-03-21 23:04:23 +0300

    Update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,14 +8,12 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-//        userService.saveUser("Fedor", "Drozdov", (byte) 23);
-//        userService.saveUser("Brad", "Pitt", (byte) 56);
-//        userService.saveUser("Kirl", "Johanson", (byte) 35);
-//        userService.saveUser("Crocodilo", "Bombardilo", (byte) 5);
-//        userService.getAllUsers();
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
-        Util.closeConnection();
+        userService.saveUser("Fedor", "Drozdov", (byte) 23);
+        userService.saveUser("Brad", "Pitt", (byte) 56);
+        userService.saveUser("Kirl", "Johanson", (byte) 35);
+        userService.saveUser("Crocodilo", "Bombardilo", (byte) 5);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
         userService.dropUsersTable();
     }
 }
